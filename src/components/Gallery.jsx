88e57d01cbc10511d0e644ad85d575416e9afb55
maelacudini <maelacudini@gmail.com>
2023-09-20 15:03:18 +0200

    new commit

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,43 +3,43 @@ import { useRef } from "react";
 
 const images = [
   {
-    url: "/gallery1.avif",
+    url: "https://images.unsplash.com/photo-1695078071178-521b9ca3289d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery2.avif",
+    url: "https://images.unsplash.com/photo-1505080463650-543249075093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery3.avif",
+    url: "https://images.unsplash.com/photo-1558690625-96d7a11c0215?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1430&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery4.avif",
+    url: "https://images.unsplash.com/photo-1694665815989-235c2049ef73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1335&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery5.avif",
+    url: "https://images.unsplash.com/photo-1610538216006-ff8246335182?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery6.avif",
+    url: "https://images.unsplash.com/photo-1611702700098-dec597b27d9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
   },
   {
-    url: "/gallery7.avif",
+    url: "https://images.unsplash.com/photo-1588964258146-787046c32910?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
     title: "some title",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, nisi nihil odit possimus illo corrupti consectetur nostrum tempore! Inventore optio dolore, officiis doloremque minima quidem voluptatum recusandae omnis molestiae autem",
